a8820d9350a87d5ec1a38a44d6a4927a32a40803
shanmukha-pasumarthi <154213169+shanmukha-pasumarthi@users.noreply.github.com>
2025-07-29 15:08:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 # Get input from the user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 # Add the numbers
 sum = num1 + num2 + num3
 
